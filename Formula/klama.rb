d91fdf51c0475ad00050df5074be2f7c83969615
Eliran Cohen <eliran89c@gmail.com>
2024-07-17 00:16:05 +0300

    Brew formula update for klama version v0.1.0

--- Formula/klama.rb
+++ Formula/klama.rb
@@ -10,7 +10,7 @@ class Klama < Formula
   on_macos do
     on_intel do
       url "https://github.com/eliran89c/klama/releases/download/v0.1.0/klama_Darwin_x86_64.tar.gz"
-      sha256 "06024862435d0e7b4b4cd51985b9557abb465bf1fab7371872dde1c1f31af6b0"
+      sha256 "19e02f5b2059e6c69923ea114d6d4e080f1b01f04064add2d551415899c685e2"
 
       def install
         bin.install "klama"
@@ -18,7 +18,7 @@ class Klama < Formula
     end
     on_arm do
       url "https://github.com/eliran89c/klama/releases/download/v0.1.0/klama_Darwin_arm64.tar.gz"
-      sha256 "36a095e92e7da57badad878eb6a88c08a54a6ed6b1a63ce9a139bb9f4520dfc7"
+      sha256 "ce7529857449f9cce5e8c54bd7f999840ed4fde555fa9a50464f5b5b1a74fa68"
 
       def install
         bin.install "klama"
@@ -30,7 +30,7 @@ class Klama < Formula
     on_intel do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/eliran89c/klama/releases/download/v0.1.0/klama_Linux_x86_64.tar.gz"
-        sha256 "302558c0689fa2cf8352e52a9a5ffee1c262dc0819330c8ab74662b0cc78d6d4"
+        sha256 "3c1e2fabc5a0361155ea2bf2c2bcc9bce08c73280b30997b1e92d73b5c70fedb"
 
         def install
           bin.install "klama"
@@ -40,7 +40,7 @@ class Klama < Formula
     on_arm do
       if Hardware::CPU.is_64_bit?
         url "https://github.com/eliran89c/klama/releases/download/v0.1.0/klama_Linux_arm64.tar.gz"
-        sha256 "1c5ee72b7b5f2bc8417058d3b1c5bfb38551433f5349cd3752332ecf8deff869"
+        sha256 "51588307e882f3a1fdc5f83c825bc2d5e51aa1024e5976daacc3bf19b0eeb5c3"
 
         def install
           bin.install "klama"
